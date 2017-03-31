3fe0944457f320f9ebdc86f3c99f71e1827fe4e9
w11p3333 <applecatkay@gmail.com>
2017-03-31 21:42:16 +0800

    update store

--- template/src/store/mutations.js
+++ template/src/store/mutations.js
@@ -1,9 +1,13 @@
 import {
-  SET_LOADING_STATUS
+  SET_LOADING_STATUS,
+  SET_DIRECTION
 } from 'src/consts'
 
 export default {
-  [SET_LOADING_STATUS] (state, { isLoading }) {
+  [SET_LOADING_STATUS] (state, isLoading) {
     state.isLoading = isLoading
+  },
+  [SET_DIRECTION] (state, direction) {
+    state.direction = direction
   }
 }
--- template/src/store/user/index.js
+++ template/src/store/user/index.js
@@ -1,39 +1,26 @@
 // ================================
 // user module
 // ================================
-import { LOGIN, LOGIN_OUT, SET_ACCESS } from 'src/consts'
+import {
+  LOGIN,
+  LOGIN_OUT
+} from 'src/consts'
 import { fetchLogin } from 'src/apis'
-import DEV_CONSTS from '../../../config/dev.consts.js'
 
 /** state */
 const state = {
   userInfo: null, // 用户基本信息
-  isLogin: false, // 是否登录
-  hasAccess: false, // 是否有权限
-  isMaster: false // 是否是管理员登录
+  isLogin: false // 是否登录
 }
 
 /** fetch login */
 const login = async ({ commit, state }, params) => {
-  if (state.userInfo) {
-    if (state.isMaster) return
-    const { permissions, admins } = state.userInfo
-    // 检查子权限 或者 某个服务的全部权限
-    if ((permissions && permissions.indexOf(params.page_name) > -1) || (admins && admins.indexOf(DEV_CONSTS.SERVICE_NAME) > -1)) commit(SET_ACCESS, true)
-    else commit(SET_ACCESS, false)
-  } else {
-    try {
-      const { data } = await fetchLogin(params)
-      const { acl_id: aclID, permissions } = data
-      if (aclID) {
-        // check page permission
-        if (permissions && permissions.indexOf(params.page_name) > -1) commit(SET_ACCESS, true)
-        else commit(SET_ACCESS, false)
-      }
-      commit(LOGIN, data)
-    } catch (e) {
-      commit(LOGIN_OUT)
-    }
+  if (state.isLogin) return
+  try {
+    const { data } = await fetchLogin(params)
+    commit(LOGIN, data)
+  } catch (e) {
+    commit(LOGIN_OUT)
   }
 }
 
@@ -44,21 +31,13 @@ const actions = {
 
 /** mutations */
 const mutations = {
-  [LOGIN] (state, userinfo) {
-    state.userInfo = userinfo
-    if (!userinfo.acl_id) {
-      state.hasAccess = true
-      state.isMaster = true
-    }
+  [LOGIN] (state, userInfo) {
+    state.userInfo = userInfo
     state.isLogin = true
   },
-  [SET_ACCESS] (state, bool) {
-    state.hasAccess = bool
-  },
   [LOGIN_OUT] (state) {
+    state.userInfo = null
     state.isLogin = false
-    state.hasAccess = false
-    state.isMaster = false
   }
 }
 
